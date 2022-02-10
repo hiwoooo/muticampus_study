b58c34b12a7d371ec3d125c8a5243fcbdc261f54
hiwoooo <95407936+hiwoooo@users.noreply.github.com>
2022-02-10 14:18:14 +0900

    커밋

--- week6/3삼육구.py
+++ week6/3삼육구.py
@@ -1,10 +1,19 @@
 #1926 369게임
 
-N=int(input())
-game=[]
-for i in range(1,N+1):
-    game.append(i)
-    
-#sfor a in game:
 
+N=int(input())
+check=['3','6','9']
+
+for i in range(1,N+1):
+  cnt=0
+  for j in str(i):
+    if j in check:
+      cnt+=1
+    
+  if cnt!=0:
+    i='-'*cnt
+
+  print(i, end=' ')
+    
+    
          

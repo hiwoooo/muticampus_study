21ccbbe435f51e99276947bcfe56a7ae970d573b
hiwoooo <95407936+hiwoooo@users.noreply.github.com>
2022-02-12 21:38:55 +0900

    커밋

---
+++ week7/3중간평균값.py
@@ -0,0 +1,10 @@
+#1984. 중간 평균값 구하기
+
+T=int(input())
+for t in range(1,T+1):
+    arr=list(map(int, input().split()))
+    arr.sort()
+
+    arr_mean=round(sum(arr[1:9])/8)
+
+    print(f'#{t} {arr_mean}')

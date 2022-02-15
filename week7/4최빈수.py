21c7e123f62652bd2ecbb4312ec76b3d2c1b074e
hiwoooo <95407936+hiwoooo@users.noreply.github.com>
2022-02-15 14:58:00 +0900

    커밋

--- week7/4최빈수.py
+++ week7/4최빈수.py
@@ -1,2 +1,19 @@
 #1204. [S/W 문제해결 기본] 1일차 - 최빈수 구하기
 
+T=int(input())
+for t in range(1,T+1):
+    a=int(input())
+    score_list=list(map(int,input().split()))
+    cnt=[0]*101
+
+    for s in score_list:
+        cnt[s]+=1
+    
+    val=max(cnt)
+
+    for i in range(101):
+        if cnt[i]==max(cnt):
+            val=i
+
+    print(f'#{t} {val}')
+

c3a98e78c17b4b43cf344d280b3988c2a93b1312
hiwoooo <95407936+hiwoooo@users.noreply.github.com>
2022-03-10 23:49:55 +0900

    커밋

--- week10/3회문2.py
+++ week10/3회문2.py
@@ -1,10 +1,14 @@
 #1216. [S/W 문제해결 기본] 3일차 - 회문2
 
-def extend_puzzle(arr, ):
-    for i in range(len(arr[0])):
-
+def findmax(puzzle, ):
+    global maxlen
+    for i in range(100):
+        for j in range(100):
+            for k in range(100,-1,-1):
+                if puzzle[i:]
 
 
 for t in range(1,11):
-    n=input()
+    puzzle=[list(input())for _ in range(100)]
+    maxlen=0
     

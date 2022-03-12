ab6cdc74d169cff50d4d1a6af16d4f0d741008a4
hiwoooo <95407936+hiwoooo@users.noreply.github.com>
2022-03-12 10:38:51 +0900

    커밋

--- week10/3회문2.py
+++ week10/3회문2.py
@@ -1,14 +1,20 @@
 #1216. [S/W 문제해결 기본] 3일차 - 회문2
 
-def findmax(puzzle, ):
+def findmax(puzzle):
     global maxlen
     for i in range(100):
         for j in range(100):
-            for k in range(100,-1,-1):
-                if puzzle[i:]
-
+            for k in range(100,1,-1):
+                if puzzle[i][j:k]==puzzle[i][k-1:j-1:-1]:
+                    if maxlen<k-j:
+                        maxlen=k-j
 
 for t in range(1,11):
     puzzle=[list(input())for _ in range(100)]
     maxlen=0
     
+    findmax(puzzle)
+    zippuzzle=list(map(list, zip(*puzzle)))
+    findmax(zippuzzle)
+
+    print(f'#{t} {maxlen}')
---
+++ week10/5.py
@@ -0,0 +1,2 @@
+#11688 Calkin-Wilf tree 1
+

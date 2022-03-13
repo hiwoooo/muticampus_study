2ef2bb2e184fb116a3f1d0a58fbf1855e627e83a
hiwoooo <95407936+hiwoooo@users.noreply.github.com>
2022-03-13 23:42:27 +0900

    커밋

--- week10/5.py
+++ week10/5.py
@@ -1,2 +1,18 @@
 #11688 Calkin-Wilf tree 1
 
+T=int(input())
+
+for t in range(1,T+1):
+    TreeNode=input()
+    a=1
+    b=1
+
+    for i in range(len(TreeNode)+1):
+        if TreeNode[i:i+1]=='L':
+            a=a
+            b=a+b
+        elif TreeNode[i:i+1]=='R':
+            a=a+b
+            b=b
+
+    print(f'#{t} {a} {b}')    

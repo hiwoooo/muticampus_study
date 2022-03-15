71d33800bbfbd268f3269d7b475d8bbd278387bf
hiwoooo <95407936+hiwoooo@users.noreply.github.com>
2022-03-16 01:20:42 +0900

    커밋

--- week11/5GNS.py
+++ week11/5GNS.py
@@ -2,16 +2,18 @@
 
 T=int(input())
 
-for i in range(1,T+1):
-    N=int(input())
+for t in range(1,T+1):
+    N,len=input().split()
     num_series=list(map(str,input().split()))
-    num_str={"ZRO":0, "ONE":1, "TWO":2, "THR":3, "FOR":4, "FIV":5 , "SIX":6, "SVN":7, "EGT":8, "NIN":9}
+    str_num={"ZRO":0, "ONE":1, "TWO":2, "THR":3, "FOR":4, "FIV":5, "SIX":6, "SVN":7, "EGT":8, "NIN":9}
+    num_str={v:k for k,v in str_num.items()}
     change=[]
 
     for n in num_series:
-        change.append(num_str[n])
+        change.append(str_num[n])
 
     change.sort()
+    change=list(num_str.get(x) for x in change)
 
-    for n in change:
-        num_str.keys(n)
+    print(f'#{t}')
+    print(*change)
